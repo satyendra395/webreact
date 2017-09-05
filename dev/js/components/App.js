dd1889f4dc28f54935084256328adcada7a1a596
Surender <surender@webcontentor.com>
2017-09-05 18:51:38 +0530

    implemented the props and state condition

--- dev/js/components/App.js
+++ dev/js/components/App.js
@@ -1,10 +1,17 @@
 import React from 'react';
+import Home from './Home';
+import Header from './Header';
 
 export default class App extends React.Component{
     render(){
+        var users = {
+            Hobbies: ['dancing', 'eating']
+        };
         return(
             <div>
                 Test
+                <Home name={"shrangali"} initialAge={24} user = {users} status={0}/>
+                <Header/>
             </div>
         );
     }
